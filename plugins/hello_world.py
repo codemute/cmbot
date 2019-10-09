5b4fab9a217a86a574f4c6bb617fabc2f6810343
Abhinav Kaushlya <abhinavkaushlya@gmail.com>
2019-10-09 16:21:58 +0530

    Single use Hello world

--- plugins/hello_world.py
+++ plugins/hello_world.py
@@ -8,15 +8,22 @@ class hello_world(BotPlugin):
     The welcome message for the new users of the community.
     """
 
+    def activate(self):
+        super().activate()
+        self.hello_world_users = set()
+
     def callback_message(self, msg):
         """
         Invite the user whose message includes the holy 'hello world'
         """
         if re.search(r'hello\s*,?\s*world', msg.body, flags=re.IGNORECASE):
             user = msg.frm.nick
-            response = tenv().get_template(
-                'hello-world.jinja2.md'
-            ).render(
-                target=user,
-            )
-            self.send(msg.frm, response)
+            if user not in self.hello_world_users:
+                response = tenv().get_template(
+                    'hello-world.jinja2.md'
+                ).render(
+                    target=user,
+                )
+                self.send(msg.frm, response)
+                self.hello_world_users.add(user)
+                print(self.hello_world_users)
--- tests/hello_world_test.py
+++ tests/hello_world_test.py
@@ -1,3 +1,5 @@
+import queue
+
 from tests.isolated_testcase import IsolatedTestCase
 
 
@@ -5,5 +7,5 @@ class HelloWorldTest(IsolatedTestCase):
 
     def test_hello_world_callback(self):
         self.assertCommand('hello world', 'Welcome to codemute!')
-        self.assertCommand('Hello World', 'Welcome to codemute!')
-        self.assertCommand('hello, world', 'Welcome to codemute!')
+        with self.assertRaises(queue.Empty):
+            self.assertCommand('helloworld', 'Welcome to codemute!')
